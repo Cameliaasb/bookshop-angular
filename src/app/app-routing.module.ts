b7bf9eab9d55bca14277a401062b4dee5c70c3e2
Camelia K <camelia.assobhei@gmail.com>
2024-02-28 09:57:28 +0100

    Routes with modules and chilren

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ListbooksComponent } from './books/listbooks/listbooks.component';
 import { AboutComponent } from './about/about.component';
-import { AddBookComponent } from './books/add-book/add-book.component';
-import { EditBookComponent } from './books/edit-book/edit-book.component';
 
 const routes: Routes = [
   // une route, "un path" = un composant + une URL
-  // Dès qu'un path est appelé, le composant est chargé
-  {path: "books", component : ListbooksComponent},
-  {path: "books/add", component: AddBookComponent},
-  {path: "books/edit", component: EditBookComponent},
+  // Dès qu'un path est appelé, le module / composants est chargé
+  // après avoir config les routes la première fois => aller dans app.component.html et y rajouter
+  // <router-outlet> </router-outlet>
+
+
+  // Appeler des modules
+  {path:"books", loadChildren: () =>  import('./books/books.module').then(m=> m.BooksModule)},
+  {path:"authors", loadChildren: () =>  import('./authors/authors.module').then(m=> m.AuthorsModule)},
+
+  // Appeler des composants
   {path: "about", component: AboutComponent},
+
+  // Route par défaut. pathMatch full pour éviter que ça matche avec une partie
   {path: "", redirectTo: "books", pathMatch: "full"}
 
-  // après avoir config les routes => aller dans app.component.html et y rajouter
-  // <router-outlet> </router-outlet>
 ];
 
 @NgModule({
--- src/app/books/books-routing.module.ts
+++ src/app/books/books-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListbooksComponent } from './listbooks/listbooks.component';
+import { AddBookComponent } from './add-book/add-book.component';
+import { EditBookComponent } from './edit-book/edit-book.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component : ListbooksComponent},
+  {path: "books/add", component: AddBookComponent},
+  {path: "books/edit", component: EditBookComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
